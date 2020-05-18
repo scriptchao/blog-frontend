6ec96300dd58a268adf332d6dd142587910dbe33
托尼 <yinchao.zhou@hipac.cn>
2020-05-18 23:31:55 +0800

    feat: test

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
  * @Author: 托尼
  * @Date: 2020-05-14 16:11:21
  * @LastEditors: 托尼
- * @LastEditTime: 2020-05-18 23:26:56
+ * @LastEditTime: 2020-05-18 23:31:36
  */ 
 import React from 'react';
 import ReactDom from 'react-dom';
@@ -28,5 +28,5 @@ ReactDom.render(
 //     module.hot.accept()
 // }
 
-console.log(10)
+console.log(11)
 
