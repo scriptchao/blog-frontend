85eee912cda6e8813b9f3138a9bf46f8cd843543
托尼 <yinchao.zhou@hipac.cn>
2020-05-20 19:12:27 +0800

    feat: 线上地址修改

--- src/stores/xhr/config.js
+++ src/stores/xhr/config.js
@@ -2,13 +2,13 @@
  * @Author: 托尼
  * @Date: 2020-05-14 16:11:22
  * @LastEditors: 托尼
- * @LastEditTime: 2020-05-14 17:40:10
+ * @LastEditTime: 2020-05-20 19:08:04
  */ 
 /**
  * Created by scriptchao on 2017/11/2.
  */
 
-const origin = 'http://127.0.0.1:7070';
+const origin = process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:7070' : 'http://121.43.224.240:7070'
 
 const expiredTime = 1000 * 60 * 125;
 
